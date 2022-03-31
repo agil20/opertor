a7bc4adbe0f7a1a1ab4b10bc6627ae1055f9de51
agil20 <agilakh@code.edu.az>
2022-03-31 16:00:14 +0400

    feat:add enum

--- Operator/Operator/Program.cs
+++ Operator/Operator/Program.cs
@@ -2,11 +2,49 @@
 
 namespace Operator
 {
+    public enum Currecly { Dollar, Euro, Lire }
     internal class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello World!");
+
         }
+        public static double Exchange(Enum obj, double azn)
+        {
+            if (obj is Enum)
+            {
+                switch (obj)
+                {
+                    case Currecly.Dollar:
+                        {
+                            azn = azn / 1.7;
+                            break;
+                        }
+                    case Currecly.Lire:
+                        {
+                            azn = azn * 8;
+                            break;
+                        }
+                    case Currecly.Euro:
+                        {
+                            azn = azn / 2;
+                            break;
+                        }
+
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("obj enumda yoxdu");
+            }
+            return azn;
+
+        }
+
+
     }
 }
+
