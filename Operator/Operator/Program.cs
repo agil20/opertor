a95c04c7fe626716529da424bf6875483688fbf3
agil20 <agilakh@code.edu.az>
2022-03-31 15:17:34 +0400

    feat:add hw

---
+++ Operator/Operator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
